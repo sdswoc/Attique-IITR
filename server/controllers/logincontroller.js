38d9bd170d5ecc775bd9c3a8f694587a020aecb7
Darshan Kumar <d_kumar@ch.iitr.ac.in>
2022-02-22 00:07:26 +0530

    add sessions

--- server/controllers/logincontroller.js
+++ server/controllers/logincontroller.js
@@ -3,15 +3,36 @@ const db = require("D:/Attique-IITR/database"); //check path
 const express = require("express");
 const bcrypt = require("bcrypt");
 const { use } = require("../routes/loginroute.js");
+const session=require("express-session")
+const cookie=require("cookie-parser")
+const mysqlStore = require('express-mysql-session')(session);
+const sessionStore = new mysqlStore(db.options);
+
 const app = express();
 app.use(express.json());
+
+
+app.use(cookie())
+
+app.use(session({
+  name: process.env.SESS_NAME,
+  resave : false,
+  saveUninitialized : false,
+  store:sessionStore,
+  secret:process.env.SESS_SECRET,
+  cookie : {
+    httpOnly: true,
+    maxAge: 1000*60*60*24 //1 day
+  }
+}))
+
 
 //login
 exports.login = (req, res) => {
   console.log("User Detected");
   res.sendFile("D:/Attique-IITR/index.html"); //static path
 };
-//logincheck without hashing
+//logincheck hashing
 exports.loginsubmit = (req, res) => {
   console.log("Login Attempted");
   console.log(req.body);
@@ -27,6 +48,7 @@ exports.loginsubmit = (req, res) => {
         } else {
           const verified = bcrypt.compareSync(reqpassword, rows[0]["pass"]);
           if (verified) {
+            console.log(session);
             res.redirect("/acad");
           } else {
             res.send("Incorrect Enrollment number or password");
